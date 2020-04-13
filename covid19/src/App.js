196cd84210b4fe237bd6922976a8b0899849a341
Huy Ho <tuanhuy0496@yahoo.com>
2020-04-13 20:05:03 +0700

    Chart and CountryPicker

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -11,6 +11,7 @@ import {fetchData} from './api/';
 class App extends React.Component {
     state = {
         data: {},
+        country: '',
     }
 
     async componentDidMount() {
@@ -19,15 +20,23 @@ class App extends React.Component {
         this.setState({data: fetchedData});
     }
 
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({data: fetchedData, country: country});
+        
+    }
+
     render(){
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return(
             
+            
             <div className={styles.container}>
                 <Cards data={data}/>
-                <CountryPicker/>
-                <Chart/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
                 
             </div>
         )
