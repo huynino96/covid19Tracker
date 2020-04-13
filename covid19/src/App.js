9ab0cad36e6b315811273a9dd1849a594c6bdefe
Huy Ho <tuanhuy0496@yahoo.com>
2020-04-13 20:27:46 +0700

    Finalise the Project

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -8,6 +8,8 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api/';
 
+import coronaImage from './image/image.png'
+
 class App extends React.Component {
     state = {
         data: {},
@@ -32,8 +34,8 @@ class App extends React.Component {
 
         return(
             
-            
             <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19"/>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data={data} country={country}/>
