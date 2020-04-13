1209ae34ede1d523f92e8abf772b0eab4ad8cd76
Huy Ho <tuanhuy0496@yahoo.com>
2020-04-13 18:40:11 +0700

    Design Cards

--- covid19/src/api/index.js
+++ covid19/src/api/index.js
@@ -4,10 +4,26 @@ const url = "https://covid19.mathdro.id/api";
 
  export const fetchData = async() => {
     try{
-        const respone = await axios.get(url);
-        
-        return respone;
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
+
+       const modifiedData = {
+           confirmed: confirmed,
+           recovered: recovered,
+           deaths: deaths,
+           lastUpdate: lastUpdate,
+       }  
+        return modifiedData;
     } catch (error){
         
+    }
+}
+
+export const fetchDailyData = async () => {
+    try{
+        const {data} = await axios.get(`{$url}/daily`);
+
+        console.log(data)
+    }catch(error){
+
     }
 }
--- covid19/src/components/Chart/Chart.jsx
+++ covid19/src/components/Chart/Chart.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
 
 const Chart = () => {
+
+    const[dailyData, setDailyData] = useState({});
+
+    useEffect(() => { 
+        const fetchAPI = async () => {
+            setDailyData( await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
     return(
         <h1>Chart</h1>
     )
