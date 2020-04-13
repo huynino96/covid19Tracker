c7e44b8751816e24aa1d1bae9a62fb0118f1be62
Huy Ho <tuanhuy0496@yahoo.com>
2020-04-13 17:44:07 +0700

    Create component structure

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
 class App extends React.Component {
     render(){
         return(
